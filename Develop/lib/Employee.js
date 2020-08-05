cb2c1a5f710d01359816c10ebc252186ac3d9732
Dasa Riadi <DASA.RIADI97@GMAIL.COM>
2020-08-05 10:29:54 -0400

    commit

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -4,6 +4,7 @@ class Employee {
     constructor(name, id, email){
         this.name = name;
         this.id = id;
+        this.title = 'Employee';
         this.email = email;
     }
     getName(){
@@ -16,7 +17,7 @@ class Employee {
         return this.email
     };
     getRole(){
-        return "Employee"
+        return this.title;
     };
 };
 
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -5,14 +5,17 @@ let Employee = require("./Employee.js");
 class Manager extends Employee{
     constructor(name, id, email, officeNumber){
         super(name, id, email);
+        this.title = 'Manager';
         this.officeNumber = officeNumber;
     };
 
     getRole(){
-        return "Manager"
+        return this.title;
     };
 
     getOfficeNumber(){
         return this.officeNumber
     };
 };
+
+module.exports = Manager;
